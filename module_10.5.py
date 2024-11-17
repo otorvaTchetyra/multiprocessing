c4bc2d7434ee0327f023c0633eea0b97a0c371a6
Daniil_Tchetyra <defender.0599@gmail.com>
2024-11-17 12:00:12 +0300

    Update module_10.5.py

--- module_10.5.py
+++ module_10.5.py
@@ -16,7 +16,7 @@ def read_info(filename):
 
 if __name__ == '__main__':
     filenames = [
-        r'D:\PythonProjects\module_10.5\file 1.txt', './file 2.txt', './file 3.txt', './file 4.txt'
+        r'D:\PythonProjects\module_10.5\multiprocessing\file 1.txt', './file 2.txt', './file 3.txt', './file 4.txt'
     ]
 
     # Линейный вызов
